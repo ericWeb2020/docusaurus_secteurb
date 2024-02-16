4db311c9841b27ec81ad74c06395948ea0a2d8e4
eric forget <eric.forget.prog@gmail.com>
2024-02-16 09:19:37 -0500

    Deploiement du docusaurus

---
+++ docs/index.md
@@ -0,0 +1,12 @@
+---
+slug: /
+title: Accueil
+sidebar_position: 1
+---
+# Bonjour,
+
+Je vous souhaite la bienvenue dans la grande famille des **Entrepôts 6R1**.
+
+Les nouveaux membres de l'équipe **Maintenance - Secteur B** sont **Béatrice Hardy** et **Bernard Cool**.
+
+**Marco Drouin**
---
+++ docusaurus.config.js
@@ -0,0 +1,82 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Entrepôt 6R1',
+  tagline: 'Dinosaurs are cool',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://github.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/docusaurus_secteurb/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'ericWeb2020', // Usually your GitHub org/user name.
+  projectName: 'docusaurus_secteurb', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: true,
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'fr',
+    locales: ['fr'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          routeBasePath:'/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Entrepôts 6R1',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+        ],
+        copyright: `Eric Forget, Alec Maltais - 2024 - 6R1`
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
